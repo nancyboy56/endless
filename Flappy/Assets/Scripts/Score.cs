070f45f5084bdd56137fb51e6a21140831bbeead
Casey <gooby5656@gmail.com>
2026-02-26 16:02:44 +1100

    display score

--- Flappy/Assets/Scripts/Score.cs
+++ Flappy/Assets/Scripts/Score.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
+using TMPro;
 
 public class Score : MonoBehaviour
 {
     [SerializeField] private int score = 0;
+    [SerializeField] private TextMeshProUGUI scoreText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        scoreText.text = score.ToString();
     }
 
     // Update is called once per frame
@@ -18,5 +20,6 @@ public class Score : MonoBehaviour
     public void AddScore()
     {
         score++;
+        scoreText.text = score.ToString();
     }
 }
