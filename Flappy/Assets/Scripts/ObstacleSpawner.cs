6c3263bbb7b5ae98a804bab78fdf7da143165857
Casey <gooby5656@gmail.com>
2026-04-08 12:33:56 +1000

    updated the obstacle spwaning script

--- Flappy/Assets/Scripts/ObstacleSpawner.cs
+++ Flappy/Assets/Scripts/ObstacleSpawner.cs
@@ -1,22 +1,32 @@
+using System;
 using UnityEngine;
 using System.Collections;
 
+
 public class ObstacleSpawner : MonoBehaviour
 {
-    [SerializeField]
-    GameObject obstaclePrefab;
+    [SerializeField] private GameObject[] obstaclePrefab;
 
-    [SerializeField, Range(0,10)]
-    float delay = 3.5f;
+    [SerializeField, Range(0,2)]
+    // startoing delay 
+    private float minDelay = 0.5f;
+
+
+    [SerializeField, Range(0,15)] private float maxDelay = 7f;
+    [SerializeField, Range(0, 10)] private float baseDelay = 2f;
+    [SerializeField, Range(0,2)] private float interval = 0.5f;
     
     [SerializeField] private float groundHeight;
+
+    [SerializeField] private float delay = 0f;
     
+    //[Se]
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //Instantiate(pillar, transform.position, Quaternion.identity);
 
-        StartCoroutine(SpawnLoop());
+        StartCoroutine(SpawnLoop()); 
     }
 
 
@@ -27,13 +37,19 @@ public class ObstacleSpawner : MonoBehaviour
         //feels unreadable
         while (true)
         {
-            //spawns at the y height of the ground
-            // i think the scaling it differently is bad so i think I will just draw different assets
-            //unless i can make a readjust height asset using a shader
-            //i could do that
-            GameObject newPillar = Instantiate(obstaclePrefab, new Vector3(transform.position.x, groundHeight,transform.position.z ), Quaternion.identity);
+            // this means that objects could come way to first too early.
+            // i think the min has to start higher over time
+            delay = UnityEngine.Random.Range(minDelay + baseDelay, maxDelay);
+            maxDelay -= interval;
+
+            // make sure the basedaly doesnt get less than 0
+            if (baseDelay - interval / 2 > 0)
+            {
+                baseDelay -= interval/2;
+            }
             
-           // might make this delay a bit random idk yet
+            GameObject newPillar = Instantiate(obstaclePrefab[0], new Vector3(transform.position.x, groundHeight,transform.position.z ), Quaternion.identity);
+            
             yield return new WaitForSeconds(delay);
         } 
     }
